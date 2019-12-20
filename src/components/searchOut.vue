e1422dee9a06d46a0ed2fc7404256caa7e54a691
webfather <1257332692@qq.com>
2019-12-20 21:52:15 +0800

    电商后台 仿 vue-element-admin

---
+++ src/components/searchOut.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="searchOut">
+        <el-button type="success" @click="cablickFn" plain>返回</el-button>
+        
+        <img src="../assets/searchOut.jpg"  style="height:100%;width:100%" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    methods:{
+        cablickFn(){
+            history.back()
+        }
+    }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
